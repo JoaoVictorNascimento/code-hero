8d4c5a13042821164cae85fa6bc6adc0a5acaad2
JoaoVictorNascimento <joao_victorxy@hotmail.com>
2023-10-07 11:13:37 -0300

    feat: create loader component and add in Characters Table content

--- src/components/charactersTable/CharactersTable.tsx
+++ src/components/charactersTable/CharactersTable.tsx
@@ -3,20 +3,59 @@ import CharacterItem from "./characterItem/CharacterItem";
 import { CharacterType } from "../../types/CharacterItem";
 import InputSearch from "../inputSearch/InputSearch";
 import { useNavigate } from 'react-router-dom';
+import Loader from '../loader/Loader';
 
 interface CharactersTableProps {
   characters?: CharacterType[],
   onSearchedValue: React.Dispatch<React.SetStateAction<string>>
+  loading: boolean;
+  hasError: boolean;
 }
 
 export default function CharactersTable({
   characters,
-  onSearchedValue
+  onSearchedValue,
+  loading,
+  hasError,
 }: CharactersTableProps) {
   const navigate = useNavigate();
 
   const navigateToCharactersDetails = (id: number, name: string) => {
-    navigate(`/${id}`, {state: {characterName: name}})
+    navigate(`/${id}`, { state: { characterName: name } })
+  };
+
+  const renderTable = () => {
+
+    if (loading) {
+      return (
+        <Loader />
+      );
+    }
+
+    return (
+      <table className={styles.charactersTable}>
+        <thead>
+          <tr>
+            <th>Personagem</th>
+            <th>Séries</th>
+            <th>Eventos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {characters && characters.map((item: CharacterType) => (
+            <CharacterItem
+              id={item.id}
+              key={item.id}
+              name={item.name}
+              thumbnail={item.thumbnail}
+              series={item.series}
+              events={item.events}
+              onClickTableRow={navigateToCharactersDetails}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
   };
 
   return (
@@ -26,30 +65,7 @@ export default function CharactersTable({
         label="Nome do personagem"
         onSearchedValue={onSearchedValue}
       />
-      <div className={styles.teste}>
-        <table className={styles.charactersTable}>
-          <thead>
-            <tr>
-              <th>Personagem</th>
-              <th>Séries</th>
-              <th>Eventos</th>
-            </tr>
-          </thead>
-          <tbody>
-            {characters && characters.map((item: CharacterType) => (
-              <CharacterItem
-                id={item.id}
-                key={item.id}
-                name={item.name}
-                thumbnail={item.thumbnail}
-                series={item.series}
-                events={item.events}
-                onClickTableRow={navigateToCharactersDetails}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {renderTable()}
     </div>
   );
 }
--- src/pages/CharactersPage/CharatersPage.tsx
+++ src/pages/CharactersPage/CharatersPage.tsx
@@ -1,14 +1,14 @@
+import { useEffect, useState } from "react";
 import Title from "../../components/layout/title/Title";
 import CharactersTable from '../../components/charactersTable/CharactersTable';
 import styles from './CharactersPage.module.scss';
 import Pagination from "../../components/pagination/Pagination";
-import { useEffect, useState } from "react";
 import { CharacterResponse } from "../../types/CharacterResponse";
 import Api from "../../services/api";
-import '../../theme/sizes.scss';
 import useIsCurrentScreenWidth from "../../hooks/useIsCurrentScreenWidth/useIsCurrentScreenWidth";
 import { usePaginationContext } from "../../providers/PaginationProvider/PaginationProvider";
 import useDebounce from "../../hooks/useDebounce/useDebounce";
+import Loader from "../../components/loader/Loader";
 
 const ITEMS_PER_PAGE = 10;
 const BUTTONS_PAGE_TO_SHOW_FULLSCREEN = 5;
@@ -17,23 +17,33 @@ const DELAY = 800;
 const MOBILE_WIDTH = 600;
 
 export default function CharactersPage() {
-  const isCurrentScreenWidth = useIsCurrentScreenWidth(MOBILE_WIDTH);
   const [charactersResponse, setCharactersResponse] = useState<CharacterResponse>();
-  const [filteredCharacter, setFilteredCharacter] = useState("");
-  const { paginationState, setPaginationState } = usePaginationContext();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [filteredCharacter, setFilteredCharacter] = useState<string>("");
   const debouncedSearchCharater = useDebounce(filteredCharacter, DELAY);
+  const { paginationState, setPaginationState } = usePaginationContext();
+  const isCurrentScreenWidth = useIsCurrentScreenWidth(MOBILE_WIDTH);
 
   useEffect(() => {
     if (paginationState.currentPage) {
       const offset = (paginationState.currentPage - 1) * ITEMS_PER_PAGE;
 
+      setLoading(true);
+      setHasError(false);
       Api.getCharacters(offset, debouncedSearchCharater)
-        .then(response => setCharactersResponse(response));
+        .then(response => {
+          setCharactersResponse(response);
+        })
+        .catch(error => {
+          console.warn(error);
+          setHasError(true);
+        })
+        .finally(() => {
+          setLoading(false)
+        })
     }
-  }, [
-    paginationState.currentPage, 
-    debouncedSearchCharater, 
-  ]);
+  }, [paginationState.currentPage, debouncedSearchCharater]);
 
   useEffect(() => {
     setPaginationState({ currentPage: 1 });
@@ -45,10 +55,12 @@ export default function CharactersPage() {
     <>
       <div className={styles.charactersPage}>
         <Title text="Busca de personagens" />
-        <CharactersTable 
-          characters={charactersResponse?.data.results}
-          onSearchedValue={setFilteredCharacter}
-        />
+          <CharactersTable 
+            characters={charactersResponse?.data.results}
+            onSearchedValue={setFilteredCharacter}
+            loading={loading}
+            hasError={hasError}
+          />
       </div>
       <div className={styles.footerCharactersPage}>
         <Pagination
